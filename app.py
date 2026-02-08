6e03a72ef8976a423f857ebc3bc6c90ae59f7e21
MarceloBatistaBhz <138399173+MarceloBatistaBhz@users.noreply.github.com>
2026-02-08 12:10:33 -0300

    Commit inicial

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="CDB / FGC",
+    page_icon="💸",
+    layout="wide"
+)
+
+st.title("Escolha o investidor no menu à esquerda")
+
---
+++ verif_nome_col.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+dados = pd.read_csv('RF_mab.csv')
+print(dados.columns.tolist())
