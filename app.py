20ca41e3e1c6e9aabf4fec0a130b0e9f1eee14c5
MarceloBatistaBhz <marcelo3295@gmail.com>
2026-02-15 13:24:28 -0300

    add password protection

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import os
+
+def check_password():
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+
+    if not st.session_state.authenticated:
+        st.title("🔒 Login")
+        password = st.text_input("Senha:", type="password")
+        if st.button("Entrar"):
+            if password == os.environ.get("APP_PASSWORD", ""):
+                st.session_state.authenticated = True
+                st.rerun()
+            else:
+                st.error("Senha incorreta")
+        st.stop()
+
+check_password()
 
 st.set_page_config(
     page_title="CDB / FGC",
